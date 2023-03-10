e2f5f4a821403dc24c4f50e08bbda9c058eac92a
Emmanuel Chucks <echucks19@gmail.com>
2023-03-10 17:58:19 +0000

    update 404 page

--- src/routes/[...404].tsx
+++ src/routes/[...404].tsx
@@ -1,31 +1,21 @@
 import { A } from "solid-start"
+import { HttpStatusCode } from "solid-start/server"
 
 export default function NotFound() {
   return (
-    <main class="mx-auto p-4 text-center text-gray-700">
-      <h1 class="max-6-xs my-16 text-6xl font-thin uppercase text-sky-700">
-        Not Found
-      </h1>
-      <p class="mt-8">
-        Visit{" "}
-        <a
-          href="https://solidjs.com"
-          target="_blank"
-          class="text-sky-600 hover:underline"
-        >
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
+    <>
+      <HttpStatusCode code={404} />
+      <h1 class="text-xl font-bold text-gray-600">404: Not Found</h1>
+      <p>
+        "I told my wife she was drawing her eyebrows too high. She looked
+        surprised."
       </p>
-      <p class="my-4">
-        <A href="/" class="text-sky-600 hover:underline">
-          Home
-        </A>
-        {" - "}
-        <A href="/about" class="text-sky-600 hover:underline">
-          About Page
-        </A>
-      </p>
-    </main>
+      <A
+        href="/"
+        class="text-gray-600 underline underline-offset-4 hover:text-gray-500"
+      >
+        &larr; Go home
+      </A>
+    </>
   )
 }
